fa93dee46ac19fcd90720ad9afecfbadcc825014
Amir Makhmudov <amir-makhmudov2004@mail.ru>
2023-10-08 21:40:07 +0300

    Реализована функция генерации пароля

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,9 @@
-class Main {
+fun generateRandomPassword(length: Int): String {
+    val allowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?`~"
+    return (1..length)
+        .map { allowedChars.random() }
+        .joinToString("")
+}
+
+fun main() {
 }
